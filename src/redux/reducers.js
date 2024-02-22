ca44b7d379bf33a5cf4451a9bc88824852999b6e
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-02-22 20:56:27 +0200

    reducers separated and combined

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,54 +1,56 @@
 import { statusFilters } from "./constants";
+// Імпортуємо функцію композиції редюсерів
+import { combineReducers } from "redux";
 
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: statusFilters.all,
-  },
-};
+const tasksInitialState = [
+  { id: 0, text: "Learn HTML and CSS", completed: true },
+  { id: 1, text: "Get good at JavaScript", completed: true },
+  { id: 2, text: "Master React", completed: false },
+  { id: 3, text: "Discover Redux", completed: false },
+  { id: 4, text: "Build amazing apps", completed: false },
+];
 
-export const rootReducer = (state = initialState, action) => {
+// Відповідає лише за оновлення властивості tasks
+// Тепер значенням параметра state буде масив завдань
+const tasksReducer = (state = tasksInitialState, action) => {
   switch (action.type) {
-    case "tasks/addTask": {
-      return {
-        ...state,
-        tasks: [...state.tasks, action.payload],
-      };
-    }
+    case "tasks/addTask":
+      return [...state, action.payload];
     case "tasks/deleteTask":
-      return {
-        ...state,
-        tasks: state.tasks.filter((task) => task.id !== action.payload),
-      };
+      return state.filter((task) => task.id !== action.payload);
     case "tasks/toggleCompleted":
-      return {
-        ...state,
-        tasks: state.tasks.map((task) => {
-          if (task.id !== action.payload) {
-            return task;
-          }
-          return {
-            ...task,
-            completed: !task.completed,
-          };
-        }),
-      };
-    case "filters/setStatusFilter": {
-      return {
-        ...state,
-        filters: {
-          ...state.filters,
-          status: action.payload,
-        },
-      };
-    }
+      return state.map((task) => {
+        if (task.id !== action.payload) {
+          return task;
+        }
+        return { ...task, completed: !task.completed };
+      });
     default:
       return state;
   }
 };
+
+const filtersInitialState = {
+  status: statusFilters.all,
+};
+
+// Відповідає лише за оновлення властивості filters
+// Тепер значенням параметра state буде об'єкт фільтрів
+const filtersReducer = (state = filtersInitialState, action) => {
+  switch (action.type) {
+    case "filters/setStatusFilter":
+      return {
+        ...state,
+        status: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// Код редюсерів tasksReducer та filtersReducer
+
+export const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  filters: filtersReducer,
+});
